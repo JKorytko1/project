cc7a1b78cd3e0e1c4328ab551bbbd4b3fe111a50
PShatalov <myproph3t@rambler.ru>
2013-11-03 00:10:29 +0300

    Добавил права доступа

--- project code/IT_Project/students.php
+++ project code/IT_Project/students.php
@@ -1,19 +1,53 @@
-<?
+<?php
   include('connection.php');
 
-   $query="Select * from students";
+	$query="Select * from students";
     $result=mysql_query($query);
-    echo"<table border=2 cellpadding=0 cellspacing=0>";
-    echo"<TR><TH>Идентификатор</TH><TH>Cтудент</TH><TH>Заметки</TH><TH>Электронная почта</TH><TH>Группа</TH></TR>";
-    while($row=mysql_fetch_array($result))//берем результаты из каждой строки
-    {
-	$query2="select group_name from groups where group_id=".$row['group_id'];
-	$resultgroups=mysql_query($query2);
-	$rowgroup=mysql_fetch_array($resultgroups);
-    echo"<tr><td>".$row['student_id']."</td><td><a href=\"student_detail.php?studentId=".$row['student_id']."\">".$row['student_name']."</a></td><td>".$row['student_notes']."</td><td>".$row['student_email']."</td><td>".$rowgroup['group_name']."</td>
-	</tr>";
-    }
-    echo"</table>";
-
- 
-?>
+	
+	$query1="(SELECT l.role FROM lectors l WHERE lector_login='".$_SESSION['login']."') UNION 
+			 (SELECT s.role FROM students s WHERE student_email='".$_SESSION['login']."' OR parent_email='".$_SESSION['login']."')"; //Определяем роль		 
+	
+	$acess_result=mysql_query($query1);
+	$acess= mysql_fetch_array($acess_result);
+	$role=$acess['role'];
+	if (!empty($_SESSION['login']) && $role==1){ //Преподаватель
+		echo"<table border=2 cellpadding=0 cellspacing=0>";
+		echo"<TR><TR><TH>Cтудент</TH><TH>Группа</TH><TH>Электронная почта</TH><TH>Заметки</TH></TR>";
+		while($row=mysql_fetch_array($result)){
+			$query2="select group_name from groups where group_id=".$row['group_id'];
+			$resultgroups=mysql_query($query2);
+			$rowgroup=mysql_fetch_array($resultgroups);
+			echo"<tr><td><a href=\"student_detail.php?studentId=".$row['student_id']."\">".$row['student_name']."</a></td><td>".$rowgroup['group_name']."</td><td>".$row['student_email']."</td><td>".$row['student_notes']."</td>
+				</tr>";
+		}
+		echo"</table>";
+	}
+	elseif(!empty($_SESSION['login']) && $role==2){ //Администратор
+		echo"<table border=2 cellpadding=0 cellspacing=0>";
+		echo"<TR><TR><TH>Cтудент</TH><TH>Группа</TH><TH>Электронная почта</TH><TH>Заметки</TH></TR>";
+		while($row=mysql_fetch_array($result)){
+			$query2="select group_name from groups where group_id=".$row['group_id'];
+			$resultgroups=mysql_query($query2);
+			$rowgroup=mysql_fetch_array($resultgroups);
+			echo"<tr><td><a href=\"student_detail.php?studentId=".$row['student_id']."\">".$row['student_name']."</a></td><td>".$rowgroup['group_name']."</td><td>".$row['student_email']."</td><td>".$row['student_notes']."</td>
+			</tr>";
+		}
+		echo"</table>";
+	}
+	elseif(!empty($_SESSION['login']) && $role==3){ //Студент и родители
+		echo"<table border=2 cellpadding=0 cellspacing=0>";
+		echo"<TR><TR><TH>Cтудент</TH><TH>Группа</TH><TH>Электронная почта</TH><TH>Заметки</TH></TR>";
+		$query3="SELECT * FROM students WHERE student_email='".$_SESSION['login']."' OR parent_email='".$_SESSION['login']."'";
+		$result3=mysql_query($query3);
+		 while($row=mysql_fetch_array($result3)){
+			$query2="select group_name from groups where group_id=".$row['group_id'];
+			$resultgroups=mysql_query($query2);
+			$rowgroup=mysql_fetch_array($resultgroups);
+			echo"<tr><td><a href=\"student_detail.php?studentId=".$row['student_id']."\">".$row['student_name']."</a></td><td>".$rowgroup['group_name']."</td><td>".$row['student_email']."</td><td>".$row['student_notes']."</td>
+			</tr>";
+		 }
+		
+	}
+	else { //Кто-то другой
+		echo "<h2>У вас нет прав для просмотра этой страницы</h2>";
+	}
