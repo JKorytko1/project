f07427cc1684f32f7848bf33fbb2d44c4b90b4da
PShatalov <myproph3t@rambler.ru>
2013-11-03 00:13:36 +0300

    Права доступа добавил

--- project code/IT_Project/groups_details.php
+++ project code/IT_Project/groups_details.php
@@ -1,13 +1,19 @@
-<?
+<?php
 	include('connection.php');
 	include('index.php');
 	  $groupId=$_GET['groupId'];
+		$query1="(SELECT l.role FROM lectors l WHERE lector_login='".$_SESSION['login']."') UNION 
+			 (SELECT s.role FROM students s WHERE student_email='".$_SESSION['login']."' OR parent_email='".$_SESSION['login']."')"; //Определяем роль		 
+	
+		$acess_result=mysql_query($query1);
+		$acess= mysql_fetch_array($acess_result);
+		$role=$acess['role'];
 	  
-	  
-	  $query = "select * from groups where group_id=".$groupId;
+		$query = "SELECT * FROM groups WHERE group_id=".$groupId;
 		$result = mysql_query($query);
 		$row = mysql_fetch_array($result);
 		echo $row['group_name']; 
+		if ($role==1){
 		echo "<table border = 1>";
 		
 		echo "<tr>";
@@ -15,17 +21,62 @@
 	   echo  "<b>" ."Студенты" ."</b>";
 	   echo "</td>";
 	   echo "</tr>";
-		$queryStudent = "Select * from students where group_id=".$groupId;
+		$queryStudent = "SELECT * FROM students WHERE group_id='".$groupId."'";
 	    $result=mysql_query($queryStudent);
-        
-			while($row = mysql_fetch_array($result)){
+
+			while($row = mysql_fetch_array($result)){	
 				echo "<tr>";
 				echo"<td>";
 				echo "<a href=\"recordbook.php?studentId=".$row['student_id']."&groupId=".$row['group_id']."\">". $row['student_name']."</a>"; 	
-
 				echo"</td>";
 				echo "</tr>";
-			}			
+			}		
+				
 		
 		echo "</table>";
+		}
+		if ($role==2){
+		echo "<table border = 1>";
+		
+		echo "<tr>";
+	   echo "<td>";
+	   echo  "<b>" ."Студенты" ."</b>";
+	   echo "</td>";
+	   echo "</tr>";
+		$queryStudent = "SELECT * FROM students WHERE group_id='".$groupId."'";
+	    $result=mysql_query($queryStudent);
+
+			while($row = mysql_fetch_array($result)){	
+				echo "<tr>";
+				echo"<td>";
+				echo "<a href=\"recordbook.php?studentId=".$row['student_id']."&groupId=".$row['group_id']."\">". $row['student_name']."</a>"; 	
+				echo"</td>";
+				echo "</tr>";
+			}		
+				
+		
+		echo "</table>";
+		}
+		if ($role==3){
+		echo "<table border = 1>";
+		
+		echo "<tr>";
+	   echo "<td>";
+	   echo  "<b>" ."Студенты" ."</b>";
+	   echo "</td>";
+	   echo "</tr>";
+		$queryStudent = "SELECT * FROM students WHERE (group_id='".$groupId."' AND student_email='".$_SESSION['login']."' OR parent_email='".$_SESSION['login']."')";
+	    $result=mysql_query($queryStudent);
+
+			while($row = mysql_fetch_array($result)){	
+				echo "<tr>";
+				echo"<td>";
+				echo "<a href=\"recordbook.php?studentId=".$row['student_id']."&groupId=".$row['group_id']."\">". $row['student_name']."</a>"; 	
+				echo"</td>";
+				echo "</tr>";
+			}		
+				
+		
+		echo "</table>";
+		}
 ?>
