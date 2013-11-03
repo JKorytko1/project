fd1ac9b0841b38f8d3fb57c7672a397a7bec4119
YVakolyuk <yahacomua@gmail.com>
2013-11-03 21:44:57 +0200

    find and add statemant code, change lectors_details delete second database file

--- project code/IT_Project/lectors_details.php
+++ project code/IT_Project/lectors_details.php
@@ -31,7 +31,8 @@
 	//$gradesQuery = "Select * from grades where lector_id=".$lectorId;
 	//$resultSt=mysql_query($gradesQuery);
 	
-	$querySubject = "Select * from subjects where lector_id=".$lectorId;
+	$querySubject = "SELECT subject_title, g.group_id, group_name, s.subject_id, semester FROM subjects as s join groups_subjects as
+  gs on s.subject_id=gs.subject_id join groups as g on gs.group_id=g.group_id WHERE s.lector_id=".$lectorId;
 	$resultSubject=mysql_query($querySubject);
     while($rowSubject = mysql_fetch_array($resultSubject)){
 		
@@ -44,24 +45,16 @@
 			
 			
 			
-    $queryGroup = "SELECT subject_title, group_name FROM subjects as s JOIN
-                   groups_subjects as gs on s.subject_id = gs.subject_id JOIN groups as g on gs.group_id = g.group_id WHERE s.lector_id=".$lectorId."";
-		$resultGroup=mysql_query($queryGroup);
-		$rowGroup = mysql_fetch_array($resultGroup);
+  
 		
 		echo"<td>"
-			.$rowGroup['group_name']
+			.$rowSubject['group_name']
 			
 			."</td>";
 			
-			$querySemester = "SELECT * from groups_subjects where subject_id in(select subject_id from subjects where lector_id in(select lector_id from lectors where lector_id=".$lectorId."))";
-           
-		$resultSemester=mysql_query($querySemester);
-		
-		$rowSemester = mysql_fetch_array($resultSemester);
 		
 		echo"<td>".
-			 "<a href= \"vedomost.php?groupId=".$rowSemester['group_id']."&subjectId=".$rowSemester['subject_id']."\">".$rowSemester['semester']."</a>"
+			 "<a href= \"vedomost.php?groupId=".$rowSubject['group_id']."&subjectId=".$rowSubject['subject_id']."\">".$rowSubject['semester']."</a>"
 			
 			."</td>";
 			
